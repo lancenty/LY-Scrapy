8480540bdbbf55000d6e4a293e48038e4c8cefb6
Lancent Yuan (CSS) <v-layuan@microsoft.com>
2018-12-11 14:22:49 +0800

    add run

--- ScrapyCarib/run.py
+++ ScrapyCarib/run.py
@@ -1,7 +1,2 @@
 import os
-import json
-import platform
-postreqdata = json.loads(open(os.environ['req']).read())
-response = open(os.environ['res'], 'w')
-response.write("Python version: {0}".format(platform.python_version())) 
-response.close()
+os.system("scrapy crawl movies")
