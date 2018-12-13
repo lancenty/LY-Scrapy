8b0c43abc659bb69756bb14c940059bd7b524958
Lancent Yuan <lancent.yuan@gmail.com>
2018-12-14 01:02:04 +0800

    update pipelines. still have bug

--- ScrapyCarib/carib_scrapy/pipelines.py
+++ ScrapyCarib/carib_scrapy/pipelines.py
@@ -6,6 +6,7 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import json
 import codecs
+import scrapy
 from scrapy.pipelines.images import ImagesPipeline
 from scrapy.pipelines.files import FilesPipeline
 
@@ -43,14 +44,35 @@ class MyFilesPipeline(FilesPipeline):
     
 
     def get_media_requests(self, item, info):
-        """Returns the media requests to download"""
-        pass
-        
+        file_url = item['file_url']
+        actor = ' '.join(item['actor'])
+        meta = {
+                  'filename': '-'.join([item['site'], item['movie_id'], item['name'], actor,]),
+                }
+        yield scrapy.Request(url=file_url, meta=meta)
+        #i = 1
+        #for image_url in item['image_urls']:
+        #    filename = '{}_{}.jpg'.format(item['name_image'], i)
+        #    yield scrapy.Request(image_url, meta={'filename': filename})
+        #    i += 1
+        #return
+
+
     def file_path(self, request, response=None, info=None):
-        pass
+        return request.meta['filename'] 
 
 
 class MyImagesPipeline(ImagesPipeline):
+
+
+    def get_media_requests(self, item, info):
+        image_url = item['image_url']
+        actor = ' '.join(item['actor'])
+        meta = {
+                  'filename': '-'.join([item['site'], item['movie_id'], item['name'], actor,]),
+                }
+        yield scrapy.Request(url=image_url, meta=meta)
+
     
-    def file_path(self, request, response=None, info=None):
-        pass
+    def image_path(self, request, response=None, info=None):
+        return request.meta['filename'] 
